e51b8d2ea7787d98be695f5b06dea3546573412c
K-ilyas <ilyaskritet@gmail.com>
2023-09-06 14:07:38 +0200

    fix : done

--- pierre.py
+++ pierre.py
@@ -11,13 +11,11 @@ def questions(code) :
         case 4 :
            return input("faîtes votre choix parmi (pierre, papier, ciseaux): ")
 
-
 cpo = questions(1)
 while cpo != 'O' and cpo != 'N':
     # if cpo != 'N' : // 
     print("Je n'ai pas compris votre réponse")
     cpo = questions(1)
-
 
 if cpo == 'O':
     joueur1 = questions(2)
@@ -33,22 +31,20 @@ elif cpo == 'N':
 np = 0
 c = True
 joueur2_result = 0
-joeur1_result = 0
+joueur1_result = 0
 while c == True:
     np += 1 
-    joueur1_choice = input("{nom} faîtes votre choix parmi (pierre, papier, ciseaux): ".format(nom=joueur1))
+    joueur1_choice = input("{nom} faîtes votre choix parmi (pierre, papier, ciseaux, puit): ".format(nom=joueur1))
    
     # use a while lop instead of a il else statment
-    while  joueur1_choice != 'pierre' and joueur1_choice != 'papier' and  joueur1_choice != 'ciseaux' :
+    while  joueur1_choice != 'pierre' and joueur1_choice != 'papier' and  joueur1_choice != 'ciseaux' and joueur1_choice !='puit' :
         print("Je n'ai pas compris votre réponse")
         print("Joueur ", joueur1)
         joueur1_choice = questions(4)
-
-
     if joueur2 == 'Machine' and cpo == 'O': 
         #Ici il faudrait plutôt vérifier que cpo == 'O' autrement
         #il y a un problème si le joueur 2 s'appelle Machine !
-        joueur2_choice = ['papier','pierre','ciseaux'][random.randint(0, 2)]
+        joueur2_choice = ['papier','pierre','ciseaux','puit'][random.randint(0, 2)]
     else :
     # if joueur2 != 'Machine' and :
         print("Joueur", joueur2)
@@ -57,7 +53,7 @@ while c == True:
 
         # use a while lop instead of il else statment
 
-        while  joueur2_choice != 'pierre' and joueur2_choice != 'papier' and  joueur2_choice != 'ciseaux' :
+        while  joueur2_choice != 'pierre' and joueur2_choice != 'papier' and  joueur2_choice != 'ciseaux' and joueur2_choice !='puit':
             print("Je n'ai pas compris votre réponse")
             print("Joueur ", joueur2)
             joueur2_choice = questions(4)
@@ -70,41 +66,21 @@ while c == True:
     #On regarde qui a gagné cette manche on calcule les points et on affiche le résultat
     if joueur1_choice == joueur2_choice :
         winner = "aucun de vous, vous être ex æquo"
-        joeur1_result = joeur1_result + 0
+        joueur1_result = joueur1_result + 0
         joueur2_result = joueur2_result + 0
         
-    elif joueur1_choice == 'pierre' and joueur2_choice == 'papier' :
+    elif (joueur1_choice == 'pierre' and joueur2_choice == 'papier') or (joueur1_choice == 'papier' and joueur2_choice == 'ciseaux') or (joueur1_choice == 'ciseaux' and joueur2_choice == 'pierre') or (joueur1_choice == 'puit' and joueur2_choice == 'papier' ) or (joueur2_choice == 'puit' and joueur1_choice != 'papier' ) :
         winner = joueur2
-        joeur1_result = joeur1_result + 0
+        joueur1_result = joueur1_result + 0
         joueur2_result = joueur2_result + 1
 
-    elif joueur1_choice == 'pierre' and joueur2_choice == 'ciseaux' :
+    elif (joueur1_choice == 'pierre' and joueur2_choice == 'ciseaux' ) or  ( joueur1_choice == 'papier' and joueur2_choice == 'pierre') or (joueur1_choice == 'ciseaux' and joueur2_choice == 'papier' ) or (joueur1_choice == 'puit' and joueur2_choice != 'papier') or (joueur2_choice == 'puit' and joueur1_choice == 'papier'):
         winner = joueur1
-        joeur1_result = joeur1_result + 1
-        joueur2_result = joueur2_result + 0
-        
-    elif joueur1_choice == 'papier' and joueur2_choice == 'ciseaux' :
-        winner = joueur2
-        joeur1_result = joeur1_result + 0
-        joueur2_result = joueur2_result + 1
-
-    elif joueur1_choice == 'papier' and joueur2_choice == 'pierre' :
-        winner = joueur1
-        joeur1_result = joeur1_result + 1
-        joueur2_result = joueur2_result + 0
-
-    elif joueur1_choice == 'ciseaux' and joueur2_choice == 'pierre' :
-        winner = joueur2
-        joeur1_result = joeur1_result + 0
-        joueur2_result = joueur2_result + 1
-
-    elif joueur1_choice == 'ciseaux' and joueur2_choice == 'papier' :
-        winner = joueur1
-        joeur1_result = joeur1_result + 1
+        joueur1_result = joueur1_result + 1
         joueur2_result = joueur2_result + 0
 
     print("le gagnant est",winner)
-    print("Les scores à l'issue de cette manche sont donc",joueur1, joeur1_result, "et", joueur2, joueur2_result, "\n")
+    print("Les scores à l'issue de cette manche sont donc",joueur1, joueur1_result, "et", joueur2, joueur2_result, "\n")
 
     if np == 5 :
        c = False
@@ -118,8 +94,6 @@ while c == True:
             c = False
        else :
             c = True
-            print("Vous ne répondez pas à la question, on continue ")
-
-  
+            print("Vous ne répondez pas à la question, on continue ") 
 else :
     print("Merci d'avoir joué ! A bientôt")
