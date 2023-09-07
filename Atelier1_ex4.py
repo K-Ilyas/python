d44c7f22301dd292dbaee2ac11837b65241adca7
K-ilyas <ilyaskritet@gmail.com>
2023-09-07 15:54:32 +0200

    feat : last update

--- Atelier1_ex4.py
+++ Atelier1_ex4.py
@@ -1,3 +1,4 @@
+import datetime
 from Atelier1_ex2 import annee_bissextile
 
 print(user_questions(1))
@@ -20,3 +21,12 @@ MONTH_DAYS = {
 
 def date_est_valide(jour,mois,annee):
     return False if  (isinstance(jour, int) and jour >=1 and jour <=  MONTH_DAYS[mois]) and (isinstance(mois, int) and mois >= 1 and mois <= 12 ) and (isinstance(anee, int)and len(str(annee)) == 4 )  else True
+
+def saisie_date_naissance():
+    while True :
+     (jour,mois,annee)= (input("jour => "),input("mois => "),input("annee => "))
+     verify_date =date_est_valide(jour,mois,annee)
+     if verify_date :
+        break
+
+    return datetime.datetime(annee,mois,jour)
